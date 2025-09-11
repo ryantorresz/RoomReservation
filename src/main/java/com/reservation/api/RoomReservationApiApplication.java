a02d7744a353d7d0af83366502bebbb54ef81619
Ryan Torres <ryantorres77690@gmail.com>
2025-09-11 07:23:58 -0300

    Initial commit

---
+++ src/main/java/com/reservation/api/RoomReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.reservation.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomReservationApiApplication.class, args);
+	}
+
+}
